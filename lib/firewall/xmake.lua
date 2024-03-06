290beaba2405fe297e2f7f4b25263b2e23d2f125
David Chisnall <github@theravensnest.org>
2024-03-06 17:54:56 +0000

    Remove some unused / nonexistent include directories.

--- lib/firewall/xmake.lua
+++ lib/firewall/xmake.lua
@@ -1,8 +1,6 @@
 
 compartment("Firewall")
-  add_includedirs("../../include", "third_party/freertos-plus-tcp/source/include")
-  add_includedirs("third_party/freertos")
-  add_includedirs(path.join(sdkdir, "include/FreeRTOS-Compat"))
+  add_includedirs("../../include")
   --FIXME: The FreeRTOS compat headers need to work with this mode!
   --add_defines("CHERIOT_NO_AMBIENT_MALLOC", "CHERIOT_NO_NEW_DELETE")
   add_files("firewall.cc")
