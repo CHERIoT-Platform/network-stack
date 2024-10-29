adam-3bian/cheriot-network-stack

Deduplicate implementations of `ntohs` and `htons`.
We had copies of `ntohs` and `htons` in the firewall, the netapi, and
the TCP/IP stack, implemented differently. In practice these differences
shouldn't matter, but this is still confusing.

Signed-off-by: Hugo Lefeuvre <hugo.lefeuvre@ubc.ca>

lib/firewall/firewall.cc
@@ -11,35 +11,11 @@
 #include <timeout.h>
 #include <timeout.hh>
 #include <vector>
+#include <endianness.h>
 
 using Debug = ConditionalDebug<false, "Firewall">;
 
 #include "firewall.hh"
-
-namespace
-{
-	// TODO These should probably be in their own library.
-	uint16_t constexpr ntohs(uint16_t value)
-	{
-		return
-#ifdef __LITTLE_ENDIAN__
-		  __builtin_bswap16(value)
-#else
-		  value
-#endif
-		    ;
-	}
-	uint16_t constexpr htons(uint16_t value)
-	{
-		return
-#ifdef __LITTLE_ENDIAN__
-		  __builtin_bswap16(value)
-#else
-		  value
-#endif
-		    ;
-	}
-} // namespace
 
 namespace
 {

lib/netapi/NetAPI.cc
@@ -7,22 +7,11 @@
 #include <atomic>
 #include <debug.hh>
 #include <token.h>
+#include <endianness.h>
 
 using Debug = ConditionalDebug<false, "Network API">;
 
 #include "../firewall/firewall.hh"
-
-namespace
-{
-	uint16_t ntohs(uint16_t value)
-	{
-		return __builtin_bswap16(value);
-	}
-	uint16_t htons(uint16_t value)
-	{
-		return __builtin_bswap16(value);
-	}
-} // namespace
 
 namespace
 {

lib/tcpip/network_wrapper.cc
@@ -17,6 +17,7 @@
 #include <locks.hh>
 #include <platform-ethernet.hh>
 #include <token.h>
+#include <endianness.h>
 
 using Debug = ConditionalDebug<false, "Network stack wrapper">;
 
@@ -73,28 +74,6 @@ using CHERI::PermissionSet;
 
 namespace
 {
-	// TODO These should probably be in their own library.
-	uint16_t constexpr ntohs(uint16_t value)
-	{
-		return
-#ifdef __LITTLE_ENDIAN__
-		  __builtin_bswap16(value)
-#else
-		  value
-#endif
-		    ;
-	}
-	uint16_t constexpr htons(uint16_t value)
-	{
-		return
-#ifdef __LITTLE_ENDIAN__
-		  __builtin_bswap16(value)
-#else
-		  value
-#endif
-		    ;
-	}
-
 	/**
 	 * Returns the key with which SealedSocket instances are sealed.
 	 */

CHERIoT-Platform/network-stack

lib/firewall/firewall.cc
@@ -5,6 +5,7 @@
 #include <compartment-macros.h>
 #include <debug.hh>
 //#include <fail-simulator-on-error.h>
+#include <endianness.hh>
 #include <locks.hh>
 #include <platform-entropy.hh>
 #include <platform-ethernet.hh>
@@ -15,31 +16,6 @@
 using Debug = ConditionalDebug<false, "Firewall">;
 
 #include "firewall.hh"
-
-namespace
-{
-	// TODO These should probably be in their own library.
-	uint16_t constexpr ntohs(uint16_t value)
-	{
-		return
-#ifdef __LITTLE_ENDIAN__
-		  __builtin_bswap16(value)
-#else
-		  value
-#endif
-		    ;
-	}
-	uint16_t constexpr htons(uint16_t value)
-	{
-		return
-#ifdef __LITTLE_ENDIAN__
-		  __builtin_bswap16(value)
-#else
-		  value
-#endif
-		    ;
-	}
-} // namespace
 
 namespace
 {

lib/netapi/NetAPI.cc
@@ -6,23 +6,12 @@
 
 #include <atomic>
 #include <debug.hh>
+#include <endianness.hh>
 #include <token.h>
 
 using Debug = ConditionalDebug<false, "Network API">;
 
 #include "../firewall/firewall.hh"
-
-namespace
-{
-	uint16_t ntohs(uint16_t value)
-	{
-		return __builtin_bswap16(value);
-	}
-	uint16_t htons(uint16_t value)
-	{
-		return __builtin_bswap16(value);
-	}
-} // namespace
 
 namespace
 {

lib/tcpip/network_wrapper.cc
@@ -12,6 +12,7 @@
 #include <FreeRTOS_IP_Private.h>
 #include <NetAPI.h>
 #include <debug.hh>
+#include <endianness.hh>
 #include <function_wrapper.hh>
 #include <limits>
 #include <locks.hh>
@@ -73,28 +74,6 @@ using CHERI::PermissionSet;
 
 namespace
 {
-	// TODO These should probably be in their own library.
-	uint16_t constexpr ntohs(uint16_t value)
-	{
-		return
-#ifdef __LITTLE_ENDIAN__
-		  __builtin_bswap16(value)
-#else
-		  value
-#endif
-		    ;
-	}
-	uint16_t constexpr htons(uint16_t value)
-	{
-		return
-#ifdef __LITTLE_ENDIAN__
-		  __builtin_bswap16(value)
-#else
-		  value
-#endif
-		    ;
-	}
-
 	/**
 	 * Returns the key with which SealedSocket instances are sealed.
 	 */