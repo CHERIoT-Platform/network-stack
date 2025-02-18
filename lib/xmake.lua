ebf043efb01b90f75c7f0f2164096d260cd690e2
Nathaniel Wesley Filardo <wes.filardo@scisemi.com>
2025-02-18 15:25:11 +0000

    Common up xmake handling for IPv6 option

--- lib/xmake.lua
+++ lib/xmake.lua
@@ -2,6 +2,13 @@ option("IPv6")
     set_default(true)
     set_showmenu(true)
 
+rule("cheriot.network-stack.ipv6")
+  on_load(function (target)
+    target:add('options', "IPv6")
+    local IPv6 = get_config("IPv6")
+    target:add("defines", "CHERIOT_RTOS_OPTION_IPv6=" .. tostring(IPv6))
+  end)
+
 includes("tcpip",
          "netapi",
          "sntp",
