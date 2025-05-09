14423da3a6e728ba1f5cc41e8b57ad3075130151
Nathaniel Wesley Filardo <wes.filardo@scisemi.com>
2025-05-09 19:27:45 +0000

    xmake: add rule to get git description
    Following https://github.com/CHERIoT-Platform/cheriot-rtos/pull/527

--- lib/xmake.lua
+++ lib/xmake.lua
@@ -1,3 +1,5 @@
+local netstackdir = os.scriptdir()
+
 option("IPv6")
     set_default(true)
     set_showmenu(true)
@@ -8,6 +10,25 @@ rule("cheriot.network-stack.ipv6")
     local IPv6 = get_config("IPv6")
     target:add("defines", "CHERIOT_RTOS_OPTION_IPv6=" .. tostring(IPv6))
   end)
+
+-- Rule for making network stack git revision information available to a build target.
+--
+-- See the RTOS's cheriot.define-rtos-git-description rule for details.
+local netstack_git_description = nil
+rule("cheriot.define-network-git-description")
+	before_build_file(function(target, sourcefile, opt)
+		netstack_git_description = netstack_git_description or try {
+			function()
+				return os.iorunv("git", {"-C", netstackdir, "describe", "--always", "--dirty"}):gsub("[\r\n]", "")
+			end
+		}
+		netstack_git_description = netstack_git_description or "unknown"
+
+		local fileconfig = target:fileconfig(sourcefile) or {}
+		fileconfig.defines = fileconfig.defines or {}
+		table.insert(fileconfig.defines, ("CHERIOT_NETWORK_GIT_DESCRIPTION=%q"):format(netstack_git_description))
+		target:fileconfig_set(sourcefile, fileconfig)
+	end)
 
 includes("tcpip",
          "netapi",
